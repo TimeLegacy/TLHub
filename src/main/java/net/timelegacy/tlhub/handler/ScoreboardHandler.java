0e24a4d1060e35899267cb5e6eadf803c57e518f
piajesse <me@piajesse.com>
2019-05-23 22:35:10 -0700

    Tried to Fixed Scoreboard Flicker setup teams but the scoreboard still flickers randomly.

--- src/main/java/net/timelegacy/tlhub/handler/ScoreboardHandler.java
+++ src/main/java/net/timelegacy/tlhub/handler/ScoreboardHandler.java
@@ -9,88 +9,101 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
-import org.bukkit.scoreboard.DisplaySlot;
-import org.bukkit.scoreboard.Objective;
-import org.bukkit.scoreboard.Score;
-import org.bukkit.scoreboard.Scoreboard;
-import org.bukkit.scoreboard.ScoreboardManager;
+import org.bukkit.scoreboard.*;
 
 public class ScoreboardHandler {
 
-  private static TLHub plugin = TLHub.getPlugin();
+    private static TLHub plugin = TLHub.getPlugin();
 
-  public static void updateBoard() {
-    new BukkitRunnable() {
-      public void run() {
-        if (Bukkit.getServer().getOnlinePlayers().size() > 0) {
-          for (Player p : Bukkit.getOnlinePlayers()) {
-            p.setScoreboard(scoreBoard(p));
-          }
-        }
-      }
-    }.runTaskTimer(plugin, 0, 20);
-  }
+    public static void updateBoard() {
+        new BukkitRunnable() {
+            public void run() {
+                if (Bukkit.getServer().getOnlinePlayers().size() > 0) {
+                    for (Player p : Bukkit.getOnlinePlayers()) {
+                        p.setScoreboard(scoreBoard(p));
+                    }
+                }
+            }
+        }.runTaskTimer(plugin, 0, 20);
+    }
 
-  public static Scoreboard scoreBoard(Player player) {
-    ScoreboardManager manager = Bukkit.getScoreboardManager();
-    Scoreboard board = manager.getNewScoreboard();
-    Objective stats = board.registerNewObjective("stats", "dummy");
+    public static Scoreboard scoreBoard(Player player) {
+        ScoreboardManager manager = Bukkit.getScoreboardManager();
+        Scoreboard board = manager.getNewScoreboard();
+        Objective stats = board.registerNewObjective("stats", "dummy");
 
-    stats.setDisplaySlot(DisplaySlot.SIDEBAR);
-    stats.setDisplayName(
-        MessageUtils.colorize(ChatColor.RED + "" + ChatColor.BOLD + "TIME LEGACY"));
+        stats.setDisplaySlot(DisplaySlot.SIDEBAR);
+        stats.setDisplayName(ChatColor.RED + "" + ChatColor.BOLD + "TIME LEGACY");
 
-    Score spacer1 = stats.getScore(MessageUtils.colorize("&f"));
-    spacer1.setScore(9); // Integer only!
+        Team spacer1 = board.registerNewTeam("spacer1");
+        spacer1.addEntry("§1");
+        stats.getScore("§1").setScore(9);
 
-    Score server =
-        stats.getScore(
-            ChatColor.WHITE
+
+        String serverString = ChatColor.WHITE
                 + "Server: "
                 + ChatColor.GRAY
-                + MessageUtils.friendlyify(ServerHandler.getType(ServerHandler.getServerUID())));
-    server.setScore(8); // Integer only!
-    // TODO Fix status once API is setup
-    Score status = stats.getScore(ChatColor.WHITE + "Status: " + ChatColor.GRAY + "?");
-    status.setScore(7); // Integer only!
+                + MessageUtils.friendlyify(ServerHandler.getType(ServerHandler.getServerUID()));
+        Team server =
+                board.registerNewTeam("server");
+        server.addEntry(serverString);
+        stats.getScore(serverString).setScore(8);
 
-    Score spacer2 = stats.getScore(MessageUtils.colorize("&7"));
-    spacer2.setScore(6); // Integer only!
 
-    Score tokens =
-        stats.getScore(
-            ChatColor.WHITE
-                + "Tokens: "
-                + ChatColor.AQUA
-                + CoinHandler.getBalance(player.getName()));
-    tokens.setScore(5); // Integer only!
+        // TODO Fix status once API is setup
+        String statusString = ChatColor.WHITE + "Status: " + ChatColor.GRAY + "?";
+        Team status = board.registerNewTeam("status");
+        status.addEntry(statusString);
+        stats.getScore(statusString).setScore(7);
 
-    Score discoveries =
-        stats.getScore(
-            ChatColor.WHITE
+
+        Team spacer2 = board.registerNewTeam("spacer2");
+        spacer2.addEntry("§2");
+        stats.getScore("§2").setScore(6);
+
+
+        String discoveriesString = ChatColor.WHITE
                 + "Discoveries: "
                 + ChatColor.LIGHT_PURPLE
                 + "?" /*TODO Replace this with the api for it*/
                 + ChatColor.GRAY
                 + "/"
                 + ChatColor.LIGHT_PURPLE
-                + "23" /*TODO Replace this with the API once its done*/);
-    discoveries.setScore(4); // Integer only!
+                + "23" /*TODO Replace this with the API once its done*/;
+        Team discoveries = board.registerNewTeam("discoveries");
+        discoveries.addEntry(discoveriesString);
+        stats.getScore(discoveriesString).setScore(5);
 
-    Score keys =
-        stats.getScore(
-            ChatColor.WHITE
+
+        String tokensString = ChatColor.WHITE
+                + "Tokens: "
+                + ChatColor.AQUA
+                + CoinHandler.getBalance(player.getName());
+        Team tokens = board.registerNewTeam("tokens");
+        tokens.addEntry(tokensString);
+        stats.getScore(tokensString).setScore(4);
+
+
+        String keysString = ChatColor.WHITE
                 + "Crates: "
                 + ChatColor.GOLD
-                + CrateKeyHandler.getBalance(player.getName()));
-    keys.setScore(3); // Integer only!
+                + CrateKeyHandler.getBalance(player.getName());
+        Team keys = board.registerNewTeam("keys");
+        keys.addEntry(keysString);
+        stats.getScore(keysString).setScore(3);
 
-    Score spacer3 = stats.getScore(MessageUtils.colorize("&8"));
-    spacer3.setScore(2); // Integer only!
 
-    Score ip = stats.getScore(ChatColor.YELLOW + "play.timelegacy.net");
-    ip.setScore(1); // Integer only!
+        Team spacer3 = board.registerNewTeam("spacer3");
+        spacer3.addEntry("§3");
+        stats.getScore("§3").setScore(2);
 
-    return board;
-  }
+
+        String ipString = ChatColor.YELLOW + "play.timelegacy.net";
+        Team ip = board.registerNewTeam("ip");
+        ip.addEntry(ipString);
+        stats.getScore(ipString).setScore(1);
+
+
+        return board;
+    }
 }
