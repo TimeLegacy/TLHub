76d417d5cc8946a9ec02528cb58d1c03350c7b42
Jesse <me@piajesse.com>
2019-06-01 23:31:23 +0200

    Revert "Added code json reader (Not working) for Discovery System"
    This reverts commit 69140d8fc756007fffacf1d0d01b468a87b13301

--- src/main/java/net/timelegacy/tlhub/handler/DiscoveriesHandler.java
+++ src/main/java/net/timelegacy/tlhub/handler/DiscoveriesHandler.java
@@ -2,12 +2,11 @@ package net.timelegacy.tlhub.handler;
 
 import net.timelegacy.tlcore.datatype.AABB3D;
 import net.timelegacy.tlcore.datatype.Polygon;
-import net.timelegacy.tlcore.datatype.Zone;
 import net.timelegacy.tlhub.TLHub;
+import org.apache.commons.lang3.tuple.Pair;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
-import org.bukkit.util.Vector;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.json.JSONTokener;
@@ -19,7 +18,7 @@ import java.util.UUID;
 
 public class DiscoveriesHandler {
     private static HashMap<UUID, ArrayList<String>> playersDiscoveries = new HashMap<>();
-    private static Zone[] discoveries;
+    private static HashMap<Polygon, Pair<String, String>> discoveries = new HashMap<>();
 
     public static void setupDiscoveries() {
         //TODO load discoveries from config
@@ -32,26 +31,31 @@ public class DiscoveriesHandler {
         JSONTokener tokener = new JSONTokener(is);
         JSONObject object = new JSONObject(tokener);
 
-        JSONArray discoveriesJson = object.getJSONArray("discoveries");
-        ArrayList<Zone> discoveriesList = new ArrayList<>();
+        JSONArray discovery = object.getJSONArray("discoveries");
+        for (int i = 0; i < discovery.length(); i++) {
+            JSONObject discoveryObject = discovery.getJSONObject(i);
 
+            System.out.println(discoveryObject.getString("formalname"));
+            System.out.println(discoveryObject.getString("shortname"));
 
-        for (int i = 0; i < discoveriesJson.length(); i++) {
-            ArrayList<AABB3D> poly = new ArrayList<>();
-            JSONObject d = discoveriesJson.getJSONObject(i);
-            JSONArray boundingBoxes = d.getJSONArray("boundingboxes");
+            JSONArray boundingBoxes = discoveryObject.getJSONArray("boundingboxes");
+            for (int b = 0; b < boundingBoxes.length(); b++) {
+                JSONArray boundingBoxesJSONArray = boundingBoxes.getJSONArray(b);
 
-            for (int ii = 0; ii < boundingBoxes.length(); ii++) {
-                JSONArray center = boundingBoxes.getJSONArray(ii).getJSONArray(0);
-                JSONArray size = boundingBoxes.getJSONArray(ii).getJSONArray(1);
-                poly.add(new AABB3D(new Vector(center.getDouble(0), center.getDouble(1), center.getDouble(2)), new Vector(size.getDouble(0), size.getDouble(1), size.getDouble(2))));
+                for (int bb = 0; bb < boundingBoxesJSONArray.length(); bb++) {
+                    JSONArray locations = boundingBoxesJSONArray.getJSONArray(bb);
+
+                    for (int loc = 0; loc < locations.length(); loc++) {
+                        //get the 3 location coords
+                        double locz = locations.getDouble(loc);
+                        System.out.println(locz);
+                    }
+                }
             }
-            AABB3D[] polyArray = new AABB3D[poly.size()];
-            polyArray = poly.toArray(polyArray);
-            discoveriesList.add(new Zone(d.getString("formalname"), d.getString("formalname"), new Polygon(polyArray)));
         }
-        discoveries = new Zone[discoveriesList.size()];
-        discoveries = (discoveriesList.toArray(discoveries));
+
+        //discoveries.put(new Polygon(new AABB3D[]{new AABB3D(new Vector(94, 153.5, -125.5), new Vector(24, 29, 53))}), );
+
     }
 
     public static void playerJoin(Player player) {
@@ -66,7 +70,6 @@ public class DiscoveriesHandler {
                 if (Bukkit.getOnlinePlayers().contains(player)) {
                     discoveryMagic(player);
                 } else {
-                    playersDiscoveries.remove(player.getUniqueId());
                     cancel();
                 }
             }
@@ -75,7 +78,7 @@ public class DiscoveriesHandler {
     }
 
     public static int getTotalAvailableDiscoveries() {
-        return discoveries.length;
+        return discoveries.size();
     }
 
     public static int getExploredDiscoveries(Player player) {
@@ -83,11 +86,14 @@ public class DiscoveriesHandler {
     }
 
     private static void discoveryMagic(Player player) {
-        for (Zone z : discoveries) {
-            if (Polygon.isInside(z.getBoundingBoxes(), AABB3D.getPlayersAABB(player))) {
-                player.sendMessage(z.getFormalname());
-                if (!playersDiscoveries.get(player.getUniqueId()).contains(z.getShortName())) {
-                    playersDiscoveries.get(player.getUniqueId()).add(z.getShortName());
+        for (Polygon poly : discoveries.keySet().toArray(new Polygon[discoveries.size()])) {
+
+
+            if (Polygon.isInside(poly, AABB3D.getPlayersAABB(player))) {
+                player.sendMessage(discoveries.get(poly).getRight());
+
+                if (!playersDiscoveries.get(player.getUniqueId()).contains(discoveries.get(poly).getLeft())) {
+                    playersDiscoveries.get(player.getUniqueId()).add(discoveries.get(poly).getLeft());
                 }
                 break;
             }
