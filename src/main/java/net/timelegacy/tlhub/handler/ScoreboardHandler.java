0a4c101e531bf5e762da6d45a23d5b96548db2f8
Shaun Agostinho <shaunagostinho@gmail.com>
2019-07-12 10:16:04 -0700

    scoreboard is amazing now <3

--- src/main/java/net/timelegacy/tlhub/handler/ScoreboardHandler.java
+++ src/main/java/net/timelegacy/tlhub/handler/ScoreboardHandler.java
@@ -1,71 +1,32 @@
 package net.timelegacy.tlhub.handler;
 
+import net.timelegacy.tlcore.datatype.CustomScoreboard;
 import net.timelegacy.tlcore.datatype.PlayerProfile;
 import net.timelegacy.tlcore.handler.CoinHandler;
 import net.timelegacy.tlcore.handler.CrateKeyHandler;
 import net.timelegacy.tlcore.handler.ServerHandler;
 import net.timelegacy.tlcore.utils.MessageUtils;
+import net.timelegacy.tlcore.utils.ScoreboardUtils;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
-import org.bukkit.scoreboard.DisplaySlot;
-import org.bukkit.scoreboard.Objective;
-import org.bukkit.scoreboard.Scoreboard;
-import org.bukkit.scoreboard.Team;
 
 public class ScoreboardHandler {
 
   public static void setupScoreBoard(Player player) {
-    Scoreboard board = player.getScoreboard();
-    Objective stats = board.registerNewObjective(player.getName(), "dummy", "TimeLegacy");
+    CustomScoreboard sb = new CustomScoreboard(player, ChatColor.RED + "" + ChatColor.BOLD + "TIME LEGACY");
+    sb.create();
+    sb.setLine(0, MessageUtils.colorize("&1")); //-> Important not to leave empty, color code helps to avoid worries//
+    sb.setLine(1, ChatColor.WHITE + "Server: " + ChatColor.GRAY
+        + MessageUtils.friendlyify(ServerHandler.getType(ServerHandler.getServerUUID())));
+    sb.setLine(2, ChatColor.WHITE + "Status: ");
+    sb.setLine(3, MessageUtils.colorize("&2"));
+    sb.setLine(4, ChatColor.WHITE + "Discoveries: ");
+    sb.setLine(5, ChatColor.WHITE + "Coins: ");
+    sb.setLine(6, ChatColor.WHITE + "Crates: ");
+    sb.setLine(7, MessageUtils.colorize("&3"));
+    sb.setLine(8, ChatColor.YELLOW + "play.timelegacy.net");
 
-    stats.setDisplaySlot(DisplaySlot.SIDEBAR);
-    stats.setDisplayName(ChatColor.RED + "" + ChatColor.BOLD + "TIME LEGACY");
-
-    Team spacer1 = board.registerNewTeam("spacer1");
-    spacer1.addEntry("§1");
-    stats.getScore("§1").setScore(9);
-
-    String serverString = ChatColor.WHITE + "Server: " + ChatColor.GRAY
-        + MessageUtils.friendlyify(ServerHandler.getType(ServerHandler.getServerUUID()));
-    Team server = board.registerNewTeam("server");
-    server.addEntry(serverString);
-    stats.getScore(serverString).setScore(8);
-
-
-    String statusString = ChatColor.WHITE + "Status: ";
-    Team status = board.registerNewTeam("status");
-    status.addEntry(statusString);
-    stats.getScore(statusString).setScore(7);
-
-    Team spacer2 = board.registerNewTeam("spacer2");
-    spacer2.addEntry("§2");
-    stats.getScore("§2").setScore(6);
-
-    String discoveriesString = ChatColor.WHITE + "Discoveries: ";
-    Team discoveries = board.registerNewTeam("discoveries");
-    discoveries.addEntry(discoveriesString);
-    stats.getScore(discoveriesString).setScore(5);
-
-    String tokensString = ChatColor.WHITE + "Tokens: ";
-    Team tokens = board.registerNewTeam("tokens");
-    tokens.addEntry(tokensString);
-    stats.getScore(tokensString).setScore(4);
-
-    String keysString = ChatColor.WHITE + "Crates: ";
-    Team keys = board.registerNewTeam("crates");
-    keys.addEntry(keysString);
-    stats.getScore(keysString).setScore(3);
-
-    Team spacer3 = board.registerNewTeam("spacer3");
-    spacer3.addEntry("§3");
-    stats.getScore("§3").setScore(2);
-
-    String ipString = ChatColor.YELLOW + "play.timelegacy.net";
-    Team ip = board.registerNewTeam("ip");
-    ip.addEntry(ipString);
-    stats.getScore(ipString).setScore(1);
-
-    player.setScoreboard(board);
+    ScoreboardUtils.saveCustomScoreboard(player.getUniqueId(), sb);
   }
 
   public static void updateEverything(Player player) {
@@ -76,7 +37,8 @@ public class ScoreboardHandler {
   }
 
   public static void updateDiscoveries(Player player) {
-    player.getScoreboard().getTeam("discoveries").setSuffix(ChatColor.LIGHT_PURPLE.toString()
+    CustomScoreboard sb = ScoreboardUtils.getCustomScoreboard(player.getUniqueId());
+    sb.setLine(4, ChatColor.WHITE + "Discoveries: " + ChatColor.LIGHT_PURPLE.toString()
         + DiscoveriesHandler.getExploredDiscoveries(player)
         + ChatColor.GRAY
         + "/"
@@ -85,11 +47,15 @@ public class ScoreboardHandler {
   }
 
   public static void updateTokens(Player player) {
-    player.getScoreboard().getTeam("tokens").setSuffix(ChatColor.AQUA.toString() + CoinHandler.getBalance(player.getUniqueId()));
+    CustomScoreboard sb = ScoreboardUtils.getCustomScoreboard(player.getUniqueId());
+    sb.setLine(5,
+        ChatColor.WHITE + "Coins: " + ChatColor.AQUA.toString() + CoinHandler.getBalance(player.getUniqueId()));
   }
 
   public static void updateCrates(Player player) {
-    player.getScoreboard().getTeam("crates").setSuffix(ChatColor.GOLD.toString() + CrateKeyHandler.getBalance(player.getUniqueId()));
+    CustomScoreboard sb = ScoreboardUtils.getCustomScoreboard(player.getUniqueId());
+    sb.setLine(6,
+        ChatColor.WHITE + "Crates: " + ChatColor.GOLD.toString() + CrateKeyHandler.getBalance(player.getUniqueId()));
   }
 
   public static void updateStatus(Player player) {// TODO Fix status once API is setup
@@ -108,6 +74,7 @@ public class ScoreboardHandler {
         break;
     }
 
-    player.getScoreboard().getTeam("status").setSuffix(status);
+    CustomScoreboard sb = ScoreboardUtils.getCustomScoreboard(player.getUniqueId());
+    sb.setLine(2, ChatColor.WHITE + "Status: " + status);
   }
 }
